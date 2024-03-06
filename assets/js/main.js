91c2548698b3e6c837274fea7eebaca5ed642d48
Mate Kovacs <smith9128@hotmail.com>
2024-03-06 23:14:05 +0000

    additional products, js, css changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -319,3 +319,16 @@ function closeFullscreen() {
     fullscreenBackdrop.style.display = 'none';
   }
 }
+const container = document.querySelector('.products-list');
+// where "container" is the id of the container
+container?.addEventListener('wheel', function (e) {
+  if (e.deltaY > 0) {
+    container.scrollLeft += 100;
+    e.preventDefault();
+    // prevenDefault() will help avoid worrisome
+    // inclusion of vertical scroll
+  } else {
+    container.scrollLeft -= 100;
+    e.preventDefault();
+  }
+});
